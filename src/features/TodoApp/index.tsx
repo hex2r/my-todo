352a0f710a870b474e6e299e7c0bea7948cd6aa4
Andrii Denysiuk <hex2r@icloud.com>
2025-07-10 21:41:41 +0200

    Add TodoAppWithStoreProvider component

--- src/features/TodoApp/index.tsx
+++ src/features/TodoApp/index.tsx
@@ -1,7 +1,17 @@
 import TodoList from "./TodoList"
 import TodoActionBar from "./TodoActionBar"
+import { TodoStoreProvider } from "./context/TodoStore"
+import { generateRandomInitialTodos } from "./helpers/generateRandomInitialTodo"
 
-export default function TodoApp() {
+export default function TodoAppWithStoreProvider() {
+  return (
+    <TodoStoreProvider initialStore={generateRandomInitialTodos(6, 6)}>
+      <TodoApp />
+    </TodoStoreProvider>
+  )
+}
+
+function TodoApp() {
   return (
     <div className="flex flex-col max-w-[40rem] mx-auto gap-8 py-8 px-4">
       <h1 className="text-3xl">To Do List</h1>
